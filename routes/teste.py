e71ce178573529cd7cb05963f21673c5cf007682
Gabriel Eric <gabriel.eric@ufrpe.br>
2025-03-22 14:08:56 -0300

    Delete routes/teste.py

--- routes/teste.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-teste_route = Blueprint('Teste', __name__)
-
-@teste_route.route('/')
-def helloWorld():
-    cliente = "ASDADS"
-    return render_template('index.html', cliente = cliente)
